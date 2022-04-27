f18b979ed466fec75cf8221aec871adddfaa124b
Tomahock <eu@tomahock.com>
2022-04-27 10:30:55 +0100

    auth to get request

--- app/API/get.php
+++ app/API/get.php
@@ -1,6 +1,17 @@
 <?php
 require_once '../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable('/var/www/html/');
+$dotenv->load();
+
+if($_SERVER['HTTP_VOST_AUTH'] !== $_ENV['VOST_AUTH']){
+    $response = [
+        'msg' => 'no auth',
+        'code' => 403
+    ];
+    \Lib\Response::json($response, 403);
+    die();
+}
 
 $data = \Lib\DataStore::getLastLocations();
 
